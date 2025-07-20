94c13dc51711becdc4ef9410ac6f5d3ce8a5054c
AmritBahadurShrestha <amritshrestha1100@gmail.com>
2025-07-20 11:05:57 +0545

    Initial Commit

---
+++ Test.js
@@ -0,0 +1,24 @@
+/* Class */
+
+class Person {
+    name;
+    age;
+    phone
+
+    constructor(name, age, phone) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone
+    }
+
+    greet() {
+        console.log('Hi', this.name)
+    }
+}
+const Arish = new Person('Arish', 25, '9868821709')
+const Kiran = new Person('Kiran', 27, '9868821710')
+
+console.log(Arish)
+Arish.greet()
+console.log(Kiran)
+Kiran.greet()
